5c2fe067cb82fcc692677c86b221327cff9a805b
Clas Rydergren <clas.rydergren@liu.se>
2017-05-09 16:36:56 +0200

    Add files via upload

--- colorconfig.py
+++ colorconfig.py
@@ -6,15 +6,15 @@ colorMat[0][0] = 0      # H lower
 colorMat[0][1] = 15      # H upper
 colorMat[0][2] = 90    # S lower
 colorMat[0][3] = 255    # S upper
-colorMat[0][4] = 90    # V lower
+colorMat[0][4] = 90    # V lower 
 colorMat[0][5] = 255    # V upper
 
 # RED2
-colorMat[1][0] = 170    # H lower
-colorMat[1][1] = 180    # H upper
-colorMat[1][2] = 140    # S lower
+colorMat[1][0] = 155    # H lower 
+colorMat[1][1] = 255    # H upper
+colorMat[1][2] = 90    # S lower
 colorMat[1][3] = 255    # S upper
-colorMat[1][4] = 140    # V lower
+colorMat[1][4] = 90    # V lower
 colorMat[1][5] = 255    # V upper
 
 # BLUE
@@ -29,25 +29,29 @@ colorMat[2][5] = 255    # V upper
 colorMat[3][0] = 0      # H lower
 colorMat[3][1] = 255    # H upper
 colorMat[3][2] = 0      # S lower
-colorMat[3][3] = 255     # S upper
-colorMat[3][4] = 230    # V lower
+colorMat[3][3] = 50     # S upper
+colorMat[3][4] = 205    # V lower
 colorMat[3][5] = 255    # V upper
 
 # PURPLE
 colorMat[4][0] = 120    # H lower
-colorMat[4][1] = 140    # H upper
-colorMat[4][2] = 80     # S lower
-colorMat[4][3] = 250    # S upper
-colorMat[4][4] = 80     # V lower
-colorMat[4][5] = 250    # V upper
+colorMat[4][1] = 160    # H upper//180
+colorMat[4][2] = 50     # S lower
+colorMat[4][3] = 255    # S upper
+colorMat[4][4] = 60     # V lower
+colorMat[4][5] = 255    # V upper
 
-# YELLOW
-colorMat[5][0] = 25     # H lower
-colorMat[5][1] = 30     # H upper
-colorMat[5][2] = 100    # S lower
-colorMat[5][3] = 255    # S upper
-colorMat[5][4] = 170    # V lower
-colorMat[5][5] = 255    # V upper
+# YELLOW/Turkos
+colorMat[5][0] = 25    # H lower //25 
+colorMat[5][1] = 30     # H upper // 30 
+colorMat[5][2] = 100    # S lower // 100
+colorMat[5][3] = 255   # S upper //255
+colorMat[5][4] = 160   # V lower //160
+colorMat[5][5] = 255    # V upper //255
+
+#TURKOS
+
+
 
 # GREEN
 colorMat[6][0] = 65     # H lower
